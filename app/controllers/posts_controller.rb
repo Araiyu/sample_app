b03d2961928f005509c1bff822af8c8f82af287a
新井　雄介 <araiyusuke@araiyuusukenoMacBook-ea.local>
2019-06-06 16:41:36 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+  		# 空のモデルをビューへ渡す
+  		@post = Post.new
+  	end
+
+  def create
+  	#ストロングパラメータを使用
+  	post = Post.new(post_params)
+  	#DBへ保存する
+  	post.save
+  	#トップ画面へリダイレクト
+  	redirect_to '/top'
+  end
+  
+  	def index
+  		@posts = Post.all
+  	end
+
+  private
+  def post_params
+  	  params.require(:post).permit(:title, :body)
+ end
+end
